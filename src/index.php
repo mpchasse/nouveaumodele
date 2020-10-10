1ec024596a9afdf24eb3d232f4be00cf1940e3ac
Marie-Pier Chassé <mp_chasse@hotmail.com>
2020-10-10 19:09:15 -0400

    Initial commit

---
+++ src/index.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>St-Hubert</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src=js/script.js"></script>
+
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
+          rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet">
+
+
+
+
+</head>
+<body>
+<header class="header-page">
+    <nav>
+        <div class="menu"><a href="index.html"><img src="/images/desktop/logoettexte.png" alt="">
+        </a>
+            <label for="show-menu" class="show-menu"><img class="material-icons" src=""
+                                                           alt="menu">
+            </i></label>
+        </div>
+        <input type="checkbox" id="show-menu" class="toggle-menu">
+        <ul class="menu">
+            <li class=""><a href="index.html"><h3>À propos</h3></a></li>
+            <li class=""><a href=""><h3>Restaurants</h3></a></li>
+            <li class=""><a href=""><h3>Produits d'épicerie</h3></a></li>
+            <li class=""><a href=""><h3>Responsabilité sociale</h3></a></li>
+            <li class=""><a href=""><h3>Menu</h3></a></li>
+            <li class=""><a href="">
+                <button>Commander</button>
+            </a></li>
+            <li class=""><a href=""><h3>FR</h3></a></li>
+        </ul>
+
+    </nav>
+</header>
+<div class="hero"><img src=""/>
+    <h1>Bienvenue chez St-Hubert</h1>
+    <h2>Maîtres-rôtisseurs, assembleurs de saveurs</h2>
+</div>
+<div class="wrapper">
+    <section class="covid">
+        <div><p>Nous avons rouvert la majorité des salles à manger de nos restaurants.
+
+            Nous vous accueillons à bras ouverts et à 2 mètres de distance.
+
+            Pour assurer votre sécurité et la nôtre, nous avons mis en place des mesures dans le cadre de la
+            réouverture.
+
+            Nos services de livraison et repas à emporter sont pour leur part 100 % opérationnels.
+
+            Consulter l’état des restaurants pour connaître la situation de votre restaurant St-Hubert préféré.</p>
+        </div>
+        <div>
+            <button>États des restaurants</button>
+        </div>
+    </section>
+
+    <section>
+        <div><img src="" alt="">
+            <button>Voir les promotions</button>
+        </div>
+
+        <div><img src="" alt="">
+            <button>Commander en ligne</button>
+        </div>
+    </section>
+
+    <section class="livraison">
+        <img src="" alt="">
+    </section>
+
+    <section classs="lescartes">
+
+        <div class="unecarte">
+            <a href="">
+                <div>
+                    <img src="" alt="">
+                    <h2>Nos produits</h2>
+                </div>
+                <div><p>Toute la famille raffolera des repas savoureux, simples et rapides à préparer que vous proposent
+                    les
+                    produits
+                    St-Hubert, offerts en épicerie.
+
+                    Des marinades aux pâtés, en passant par les soupes, salades et desserts, nos plats préparés sont
+                    prêts à
+                    savourer en
+                    quelques instants.</p>
+                </div>
+            </a>
+        </div>
+
+        <div class="unecarte">
+            <a href="">
+                <div>
+                    <img src="" alt="">
+                    <h2>Nos recettes</h2>
+                </div>
+                <div><p>Vous planifiez un repas de semaine en famille, une soirée entre amis ou une occasion spéciale ?
+                    St-Hubert a plein de suggestions pour vous!
+                </p>
+                    <p>
+                        Découvrez de nouvelles façons d’apprêter les aliments St-Hubert et de surprendre vos invités
+                        avec de
+                        nouveaux plats. Vous y trouverez tout ce qu’il faut pour créer des menus savoureux et simples à
+                        préparer!
+                    </p>
+                </div>
+            </a>
+        </div>
+
+        <div class="unecarte">
+            <a href="">
+                <div>
+                    <img src="" alt="">
+                    <h2>Nos restaurants</h2>
+                </div>
+                <div><p>Renommé pour son poulet rôti à la broche et sa sauce BBQ légendaire, le nouveau St-Hubert vous
+                    réserve une expérience unique : menu à l’ardoise, bières de microbrasserie, entrées à partager,
+                    assiettes brunch et plus encore.</p>
+                </div>
+            </a>
+
+        </div>
+        <div class="unecarte">
+            <a href="">
+                <div>
+                    <img src="" alt="">
+                    <h2>Engagement social</h2>
+                </div>
+                <div><p>St-Hubert est fier de ses valeurs et de son engagement dans la communauté grâce, entre autres, à
+                    la
+                    Fondation St-Hubert.</p>
+                </div>
+            </a>
+
+        </div>
+        <div class="unecarte">
+            <a href="">
+                <div>
+                    <img src="" alt="">
+                    <h2>Carrières</h2>
+                </div>
+                <div><p>Que ce soit dans nos bureaux, restaurants St-Hubert ou usines, nous avons sûrement un emploi qui
+                    correspond à votre profil et votre choix de vie.</p>
+
+                    <p>En usant de vos talents et de vos compétences, vous contribuerez à faire de l’expérience de nos
+                        clients un moment réconfortant.mitable de votre rôtisserie locale</p>
+                </div>
+            </a>
+        </div>
+
+        <div class="unecarte">
+            <a href="">
+                <div>
+                    <img src="" alt="">
+                    <h2>Fanclub</h2>
+                </div>
+                <div><p>Vous êtes un vrai fan de St-Hubert? Vous aimeriez être au courant des nouveautés et des
+                    concours? Si
+                    oui, c’est le temps de faire partie du fan club de St-Hubert!
+                </p>
+                    <p>Abonnez-vous à notre infolettre, suivez-nous sur Facebook ou créez votre profil client sur notre
+                        site. De cette façon, vous aurez accès aux primeurs, aux concours et à des offres exclusives!
+                    </p>
+
+                </div>
+            </a>
+
+        </div>
+
+    </section>
+</div>
+
+<div class="wrapper">
+    <section class="infolettre">
+        <div>
+            <img src="" alt="">
+        </div>
+
+        <div>
+            <h2>Inscrivez-vous à l’infolettre</h2>
+            <p>Restez à l'affût des nouveautés, soyez avisés des promotions et profitez d’avantages exclusifs aux
+                abonnés.</p>
+
+            <div class="infolettre-info"><input type="email" id="courriel" name="courriel"
+                                                placeholder="Adresse courriel"
+                                                required="required">
+                <button class="btn">S'inscrire</button>
+            </div>
+        </div>
+
+    </section>
+</div>
+
+
+<footer>
+    <div class="wrapper">
+        <div>
+            <ul class="footer-menu">
+                <li class="">À propos de St-Hubert</li>
+                <li class=""><a href="">Restaurants</a></li>
+                <li class=""><a href="">Produits d'épicerie</a></li>
+                <li class=""><a href="">Responsabilité sociale</a></li>
+                <li class=""><a href="">À propos de nous</a></li>
+                <li class=""><a href="">Cartes-cadeaux</a></li>
+                <li class=""><a href="">Fan club</a></li>
+            </ul>
+        </div>
+
+        <div>
+            <ul class="footer-menu">
+                <li class="">En restaurant</li>
+                <li class=""><a href="">Menu</a></li>
+                <li class=""><a href="">Service de traiteur</a></li>
+                <li class=""><a href="">Promotions</a></li>
+                <li class=""><a href="">Coupons</a></li>
+                <li class=""><a href="">Concours</a></li>
+                <li class=""><a href="">Informations nutrionnelles</a></li>
+            </ul>
+        </div>
+
+        <div>
+            <ul class="footer-menu">
+                <li class="">Info corporative</li>
+                <li class=""><a href="">Carrières</a></li>
+                <li class=""><a href="">Nos franchises</a></li>
+                <li class=""><a href="">Notre Fondation</a></li>
+                <li class=""><a href="">Foires aux questions (FAQ)</a></li>
+                <li class=""><a href="">Donner votre opinion</a></li>
+                <li class=""><a href="">Contactez-nous</a></li>
+            </ul>
+        </div>
+
+        <div>
+            <div><h4>Suivez-nous!</h4></div>
+            <div>
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+            </div>
+
+            <div><h4>Télécharger l'application de commande</h4></div>
+            <div>
+                <img src="" alt="">
+
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <div class="fin">
+
+        <div class="footer-info">
+            <h3>Conditions d'utilisation</h3>
+            <h3>Politique de confidentialité</h3>
+            <h3>Accessibilité</h3>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
